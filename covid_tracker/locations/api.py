d2c5568066d8eab6735461e035d28db6dc19fb3e
Ricardo Hayase Koaro <ricardo.hayase98@gmail.com>
2020-11-11 12:00:23 -0300

    POST method create location
    Also parsing google locations json to valid data format

--- covid_tracker/locations/api.py
+++ covid_tracker/locations/api.py
@@ -1,14 +1,38 @@
 from locations.models import Location
-from rest_framework import viewsets, permissions
-from .serializers import LocatlionSerializer
+from rest_framework.decorators import action
+from rest_framework import viewsets, permissions, mixins
+from .serializers import LocationSerializer
+from rest_framework.response import Response
 
 # Location Viewset
 
-class LocationViewSet(viewsets.ModelViewSet):
+class LocationViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     
     permission_classes = [
         permissions.AllowAny,
     ]
-    serializer_class = LocatlionSerializer
-
+    serializer_class = LocationSerializer
     queryset = Location.objects.all()
+
+    def create(self, request, *args, **kwargs):
+         # Create a list of dict with valid key - value for location model
+        locations = request.data["timelineObjects"]
+        resp = []
+        for local in locations:
+            if "placeVisit" in local:
+                aux_local = dict()
+                aux_local["name"] = local["placeVisit"]["location"]["name"]
+                aux_local["placeId"] = local["placeVisit"]["location"]["placeId"]
+                aux_local["latitude"] = local["placeVisit"]["location"]["latitudeE7"]
+                aux_local["longitude"] = local["placeVisit"]["location"]["longitudeE7"]
+                aux_local["startTimestamp"] = local["placeVisit"]["duration"]["startTimestampMs"]
+                aux_local["endTimestamp"] = local["placeVisit"]["duration"]["endTimestampMs"]
+                serializer = self.get_serializer(data=aux_local)
+                serializer.is_valid(raise_exception=True)
+                self.perform_create(serializer)
+                resp.append(serializer.data)
+        return Response(resp)
+
+    def perform_create(self, serializer):
+        serializer.save()
+
