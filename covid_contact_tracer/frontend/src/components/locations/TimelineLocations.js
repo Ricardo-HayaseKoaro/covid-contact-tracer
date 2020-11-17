68b3ee23a229ad9af83b7baace315e53ec5d954d
Ricardo Hayase Koaro <ricardo.hayase98@gmail.com>
2020-11-17 19:36:25 -0300

    add timeline visualizer for locations

---
+++ covid_contact_tracer/frontend/src/components/locations/TimelineLocations.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Timeline from '@material-ui/lab/Timeline';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
+import Typography from '@material-ui/core/Typography';
+
+import { getLocations } from '../../actions/locations';
+import { connect } from 'react-redux';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: '6px 16px',
+  },
+  secondaryTail: {
+    backgroundColor: theme.palette.secondary.main,
+  },
+}));
+
+const convertDate = data => {
+  let d = new Date(data);
+  return d.toLocaleString();
+}
+
+const mapStateToProps = state => {
+  return {
+    locations: state.locations.locations
+  }
+}
+  
+const mapDispatchToProps = dispatch => {
+  return {
+    getLocations: () => dispatch(getLocations())
+  }
+}
+
+function TimelineLocations(props) {
+  const classes = useStyles();
+
+  return (
+    <Timeline>
+      {props.locations.map((local) => {
+        return (
+          <TimelineItem>
+            <TimelineOppositeContent>
+              <Typography>
+                {local["name"]}
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot/>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Typography color="textSecondary">
+                  {convertDate(local["startTime"])}
+              </Typography>
+            </TimelineContent>
+          </TimelineItem>
+        );
+      })}
+    </Timeline>
+  );
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TimelineLocations);
