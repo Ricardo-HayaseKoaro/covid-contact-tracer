9f4361f6b3aa5b25b1b31a70a093aab96ba7f6c3
Ricardo Hayase Koaro <ricardo.hayase98@gmail.com>
2020-12-23 04:52:52 -0300

    fix notification permission

--- covid_contact_tracer/notifications/permission.py
+++ covid_contact_tracer/notifications/permission.py
@@ -9,4 +9,4 @@ class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
        
         # Instance must have an attribute named `owner`.
-        return obj.owner == request.user
+        return obj.user == request.user
