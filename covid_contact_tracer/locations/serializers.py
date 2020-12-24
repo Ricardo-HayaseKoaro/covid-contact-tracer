327455bf9a33965ef78477951b2663d56525a172
Ricardo Hayase Koaro <ricardo.hayase98@gmail.com>
2020-12-24 02:03:42 -0300

    add notification to location serializer

--- covid_contact_tracer/locations/serializers.py
+++ covid_contact_tracer/locations/serializers.py
@@ -1,9 +1,19 @@
-
 from rest_framework import serializers
 from locations.models import Location 
+from notifications.models import Notification
+
+
+# Notification Serializer
+class NotificationSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Notification 
+    fields = ['notifier', 'id', 'created_at']
 
 # Location Serializer
 class LocationSerializer(serializers.ModelSerializer):
+  notifications = NotificationSerializer(many=True)
   class Meta:
     model = Location 
     fields = '__all__'
+
+
