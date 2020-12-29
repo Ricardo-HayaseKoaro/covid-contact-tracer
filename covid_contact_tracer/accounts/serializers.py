c90c5e5044a384249d90f47026e7cc388fdaf0bd
Ricardo Hayase Koaro <ricardo.hayase98@gmail.com>
2020-12-28 23:44:39 -0300

    create update user, change password backend

--- covid_contact_tracer/accounts/serializers.py
+++ covid_contact_tracer/accounts/serializers.py
@@ -1,30 +1,138 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
+
 
 # User Serializer
 class UserSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = User
-    fields = ('id', 'username', 'email')
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name')
+
 
 # Register Serializer
 class RegisterSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = User
-    fields = ('id', 'username', 'email', 'password')
-    extra_kwargs = {'password': {'write_only': True}}
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
 
-  def create(self, validated_data):
-    user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
-    return user
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password2',
+                  'email', 'first_name', 'last_name')
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True}
+        }
 
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."})
+
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+
+
+# Login serializer
 class LoginSerializer(serializers.Serializer):
-  username = serializers.CharField()
-  password = serializers.CharField()
+    username = serializers.CharField()
+    password = serializers.CharField()
 
-  def validate(self, data):
-    user = authenticate(**data)
-    if user and user.is_active:
-      return user
-    raise serializers.ValidationError("Incorrect Credentials")
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect Credentials")
+
+
+# Change password serializer
+class ChangePasswordSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+    old_password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('old_password', 'password', 'password2')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."})
+
+        return attrs
+
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError(
+                {"old_password": "Old password is not correct"})
+        return value
+
+    def update(self, instance, validated_data):
+
+        instance.set_password(validated_data['password'])
+        instance.save()
+
+        return instance
+
+
+# Update serializer
+class UpdateUserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+        }
+
+    def validate_email(self, value):
+        user = self.context['request'].user
+        if User.objects.exclude(pk=user.pk).filter(email=value).exists():
+            raise serializers.ValidationError(
+                {"email": "This email is already in use."})
+        return value
+
+    def validate_username(self, value):
+        user = self.context['request'].user
+        if User.objects.exclude(pk=user.pk).filter(username=value).exists():
+            raise serializers.ValidationError(
+                {"username": "This username is already in use."})
+        return value
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data['first_name']
+        instance.last_name = validated_data['last_name']
+        instance.email = validated_data['email']
+        instance.username = validated_data['username']
+
+        instance.save()
+
+        return instance
--- covid_contact_tracer/accounts/urls.py
+++ covid_contact_tracer/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .api import RegisterAPI, LoginAPI, UserAPI
+from .api import RegisterAPI, LoginAPI, UserAPI, ChangePasswordAPI, UpdateAccountAPI
 from knox import views as knox_views
 
 urlpatterns = [
@@ -7,5 +7,7 @@ urlpatterns = [
   path('api/auth/register', RegisterAPI.as_view()),
   path('api/auth/login', LoginAPI.as_view()),
   path('api/auth/user', UserAPI.as_view()),
-  path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout')
+  path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
+  path('api/auth/change_password', ChangePasswordAPI.as_view(), name='auth_change_password'),
+  path('api/auth/update_account', UpdateAccountAPI.as_view(), name='auth_update_account'),
 ]
