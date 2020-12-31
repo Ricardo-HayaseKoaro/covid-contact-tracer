0cb4ec47a3387dd39daefde32459826d201c6217
Ricardo Hayase Koaro <ricardo.hayase98@gmail.com>
2020-12-31 00:38:34 -0300

    create validation for file upload

--- covid_contact_tracer/frontend/src/components/locations/UploadLocations.js
+++ covid_contact_tracer/frontend/src/components/locations/UploadLocations.js
@@ -3,6 +3,8 @@ import { DropzoneDialog } from 'material-ui-dropzone'
 
 import { connect } from 'react-redux';
 import { uploadLocations } from '../../actions/locations';
+import { createAlert } from '../../actions/alerts';
+
 
 const mapStateToProps = state => {
     return {}
@@ -10,7 +12,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        uploadLocations: (files) => dispatch(uploadLocations(files))
+        uploadLocations: (files) => dispatch(uploadLocations(files)),
+        createAlert: (msg, type) => dispatch(createAlert(msg, type)),
     }
 }
 
@@ -32,30 +35,34 @@ class UploadLocationsDialog extends Component {
                 reader.readAsText(file);
             });
         })).then(function (results) {
-            var objs = [];
-            results.forEach(function (item) {
-                const timelineObjects = JSON.parse(item['result']);
-                objs = objs.concat(timelineObjects["timelineObjects"]);
-            });
-            objs = objs.filter((location) => Object.keys(location) == "placeVisit");
-            let locations = [];
-            objs.forEach((obj) => {
-                let location = {};
-                location["name"] = obj.placeVisit["location"]["name"];
-                location["placeId"] = obj.placeVisit["location"]["placeId"];
-                location["latitude"] = obj.placeVisit["location"]["latitudeE7"]/10000000;
-                location["longitude"] = obj.placeVisit["location"]["longitudeE7"]/10000000;
-                location["startTime"] = new Date(obj.placeVisit["duration"]["startTimestampMs"]/1).toISOString();
-                location["endTime"] = new Date(obj.placeVisit["duration"]["endTimestampMs"]/1).toISOString();
-                location["notified"] = false;
-                location["infected"] = false;
-                locations.push(location);
-            });
-            _this.props.uploadLocations((locations));
-            _this.setState({
-                open: false,
-            });
-        });
+            try {
+                var objs = [];
+                results.forEach(function (item) {
+                    const timelineObjects = JSON.parse(item['result']);
+                    objs = objs.concat(timelineObjects["timelineObjects"]);
+                });
+                objs = objs.filter((location) => Object.keys(location) == "placeVisit");
+                let locations = [];
+                objs.forEach((obj) => {
+                    let location = {};
+                    location["name"] = obj.placeVisit["location"]["name"];
+                    location["placeId"] = obj.placeVisit["location"]["placeId"];
+                    location["latitude"] = obj.placeVisit["location"]["latitudeE7"]/10000000;
+                    location["longitude"] = obj.placeVisit["location"]["longitudeE7"]/10000000;
+                    location["startTime"] = new Date(obj.placeVisit["duration"]["startTimestampMs"]/1).toISOString();
+                    location["endTime"] = new Date(obj.placeVisit["duration"]["endTimestampMs"]/1).toISOString();
+                    location["notified"] = false;
+                    location["infected"] = false;
+                    locations.push(location);
+                });
+                _this.props.uploadLocations((locations));
+                _this.setState({
+                    open: false,
+                });
+            } catch (e) {
+                _this.props.createAlert({uploadFail: "Invalid File"}, "error")
+            }
+        })
     }
 
     render() {
